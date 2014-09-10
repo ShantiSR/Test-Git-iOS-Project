9eb5e815ee7f9114d32bdfea3a92b28d6c9589c9
Shanti Rodríguez <remixn95@gmail.com>
2014-09-10 21:47:16 +0200

    added a test class

---
+++ Git Test/Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Shanti on 10/09/14.
+//  Copyright (c) 2014 Shanti Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Shanti on 10/09/14.
+//  Copyright (c) 2014 Shanti Rodriguez. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
